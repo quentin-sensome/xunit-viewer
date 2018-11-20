86518b0bbfc6a530f835039efa557a9a3bb4cab0
Quentin Cavalié <38936477+quentin-sensome@users.noreply.github.com>
2018-11-20 12:18:52 +0100

    Update search-suites.js

--- component/search-suites.js
+++ component/search-suites.js
@@ -2,7 +2,7 @@ import clone from 'clone'
 import fuzzysearch from 'fuzzysearch'
 
 let matches = (str, match) => {
-  return str.toLowerCase().split(' ').some(v => match.indexOf(v) >= 0)
+  return str.toLowerCase().split(' ').some(v => match.toLowerCase().indexOf(v) >= 0)
 }
 
 export default (suites, search) => {
